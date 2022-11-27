3ceaeb05987595859bf28a086500673152a20695
Christopher Bayliss <cjbdev@icloud.com>
2022-11-28 08:25:01 +1100

    restore custom guicursor in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,6 @@
 -- general config
 vim.o.cursorline = true
-vim.o.guicursor = ''
+vim.o.guicursor = 'n-v-c:hor20,i-ci:ver20,a:blinkwait300-blinkon200-blinkoff150'
 vim.o.hidden = true
 vim.o.number = true
 vim.o.termguicolors = true
