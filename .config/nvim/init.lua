ba7c985b59cb9cc0905c7dd15e2c7a679355a401
Christopher Bayliss <cjbdev@icloud.com>
2022-12-09 11:31:51 +1100

    initial Telescope config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -158,6 +158,22 @@ packer.startup(function()
     })
 
     use({
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.0',
+        requires = { { 'nvim-lua/plenary.nvim' } },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
+            vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+            vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+        end,
+    })
+
+    use({
         'neovim/nvim-lspconfig',
         config = function()
             local opts = { noremap = true, silent = true }
